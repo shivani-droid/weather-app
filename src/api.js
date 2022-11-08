af0bed0778acc601b804ea08038467eb17ba168b
shivani-droid <84192940+shivani-droid@users.noreply.github.com>
2022-11-08 16:32:22 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "f7610aec2bmsh26dde47a5ce7435p1c072cjsndb7864265652",
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "c7e990188322e219571ae5771383bfec"; 
+  
